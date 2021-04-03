00d62644da94ab536260c7206ca76e102919b0b6
zacard.lin <13816025562@139.com>
2021-04-03 10:08:03 +0800

    :arp: feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      123
-      {/*<header className="App-header">*/}
+      123444
       <img src={logo} className="App-logo" alt="logo" />
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
     </div>
   );
 }
