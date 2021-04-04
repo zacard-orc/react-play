962d695ac4c341800ba7534399618d3b9517dcd6
zacard.lin <13816025562@139.com>
2021-04-04 23:14:59 +0800

    :art: style: add eslint rule prefer-template

--- src/hox/counter.ts
+++ src/hox/counter.ts
@@ -5,7 +5,11 @@ function useCounter(initialValue: number) {
   const [count, setCount] = useState(initialValue ?? 0);
   const decrment = () => setCount(count - 1);
   const incrment = () => setCount(count + 1);
+  const bb = 'eslint';
+  const cc = `v => ${bb}`;
+
   return {
+    cc,
     count,
     decrment,
     incrment,
