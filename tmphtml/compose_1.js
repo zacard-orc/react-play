0f0d0dd87be7bb1b3de63d5eee30e8ba94c6d8c8
zacard.lin <13816025562@139.com>
2021-08-17 07:56:21 +0800

    :art: compose

--- tmphtml/compose_1.js
+++ tmphtml/compose_1.js
@@ -13,6 +13,11 @@ function compose(...funcs) {
     console.log('_red', prev, b, idx);
     const tmpf = (...args) => {
       console.log('tmf,', idx, args);
+      /*
+        f,g,h
+        1,g(..args) 执行，g结果最后f的入参数 f(g(...args))
+        2,f(g(h(...args)) 执行，h结果最后最为g的入参 ，再gh结果作为f的入参
+       */
       prev(b(...args));
     };
     return tmpf;
@@ -44,6 +49,8 @@ console.log(bb);
   console.log(el, idx);
 });
 
+// 核心是reduce最后一个值不带的时候overload了回调
+// 直接从idx1开始
 [1, 2, 3, 4].reduce((prev, el, idx) => {
   console.log(el, idx);
 }, []);
